67997f0d574bd1dfda51d9d49dfe1dc580e94bda
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-11-22 22:51:21 -0300

    create variables

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int quant;
+	int choice;
+	
+	const double roomPrice = 1200.0; 
+	const double pastaPrice = 250.0; 
+	const double burgerPrice = 120.0;
+	const double noodlesPrice = 140.0; 
+	const double shakePrice = 120.0; 
+	const double chickenPrice = 150.0;
+	
+	//variables that store the quantity of rooms and food consumption per rooms
+	int qRooms = 0, qPasta=0, qBurger=0, qNoodles=0, qShake=0, qChicken=0;
+	
+	//the number of rooms and itens per room from each category that has been sold
+	int sRooms=0, sPasta=0, sBurger=0, sNoodles=0, sShake=0, sChicken=0;
+	
+	//the total price of rooms or foods
+	int totalRooms=0, totalPasta=0, totalBurger=0, totalNoodles=0, totalShake=0, totalChicken=0;
+	
+	return 0;
+}
