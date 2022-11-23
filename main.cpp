12d71976077a2aba7cfe6e8888bd7c2960838c8a
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-11-22 23:13:52 -0300

    create a list of itens

--- main.cpp
+++ main.cpp
@@ -22,5 +22,32 @@ int main() {
 	//the total price of rooms or foods
 	int totalRooms=0, totalPasta=0, totalBurger=0, totalNoodles=0, totalShake=0, totalChicken=0;
 	
+	cout<<"\n\t Quantity of items we have.";
+	cout<<"\n\t Rooms available: ";
+	cin>>qRooms;
+	cout<<"\n Quantity of pasta: ";
+	cin>>qPasta;
+	cout<<"\n Quantity of burger: ";
+	cin>>qBurger;
+	cout<<"\n Quantity of noodles: ";
+	cin>>qNoodles;
+	cout<<"\n Quantity of shake: ";
+	cin>>qShake;
+	cout<<"\n Quantity of chicken-roll: ";
+	cin>>qChicken;
+	
+	cout<<"\n\t\t\t Please select from the menu options ";
+	cout<<"\n\n1) Rooms";
+	cout<<"\n2) Pasta";
+	cout<<"\n3) Burger";
+	cout<<"\n4) Noodles";
+	cout<<"\n5) Shake";
+	cout<<"\n6) Chicken-roll";
+	cout<<"\n7) Information regarding sales and collection";
+	cout<<"\n8) Exit";
+	
+	cout<<"\n\n Please Enter your choice! ";
+	cin>>choice;
+	
 	return 0;
 }
